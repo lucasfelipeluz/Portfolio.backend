b78f386e36a89769d65f02c8918a86b7f32049ae
Lucas Luz <lucasfelipeluz.dev@gmail.com>
2023-08-29 22:21:15 -0300

    feat: added viewModels

---
+++ Portfolio.API/ViewModels/CreateProjectViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.API.ViewModels
+{
+  public class CreateProjectViewModel
+  {
+    [Required(ErrorMessage = "The project description is required!")]
+    [MinLength(3, ErrorMessage = "The project description must have more than 3 letters")]
+    [MaxLength(80, ErrorMessage = "The project description must have less than 80 letters")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "The project description is required!")]
+    [MinLength(3, ErrorMessage = "The project description must have more than 3 letters")]
+    [MaxLength(80, ErrorMessage = "The project description must have less than 80 letters")]
+    public string Description { get;  set; }
+    public string UrlWebsite { get; set; }
+
+    [Required(ErrorMessage = "The project description is required!")]
+    [MinLength(3, ErrorMessage = "The project description must have more than 3 letters")]
+    [MaxLength(80, ErrorMessage = "The project description must have less than 80 letters")]
+    public string UrlGithub { get;  set; }
+
+    [Required(ErrorMessage = "The project view priority is required!")]
+    public int ViewPriority { get;  set; }
+
+    [DataType(DataType.Date)]
+    [Required(ErrorMessage = "The project Start Date is required!")]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+    public DateTime? StartedAt { get; set; }
+
+    [DataType(DataType.Date)]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+    public DateTime? FinishedAt { get; set; }
+  }
+}
