21eec1dbc9c9da11aff0e9511028946003a04cc0
Lucas Luz <lucasfelipeluz.dev@gmail.com>
2024-03-07 15:52:59 -0300

    feat: new route for home admin

--- src/Portfolio.Services/Interfaces/ISkillService.cs
+++ src/Portfolio.Services/Interfaces/ISkillService.cs
@@ -5,6 +5,7 @@ namespace Portfolio.Services.Interfaces;
 public interface ISkillService
 {
 	Task<List<SkillDto>> GetAllSkillsAsync();
+	Task<List<SkillDto>> GetAllSkillsAsync(bool isActive);
 	Task<SkillDto> GetSkillByIdAsync(int id);
 	Task<bool> CreateSkillAsync(SkillDto skillDto);
 	Task<bool> UpdateSkillAsync(SkillDto skillDto);
