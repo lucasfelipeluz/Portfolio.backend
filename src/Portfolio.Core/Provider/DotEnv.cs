8641badad59c9652b0d00c9f6d978674bf309319
Lucas Luz <lucasfelipeluz.dev@gmail.com>
2024-03-07 15:19:30 -0300

    feat: Core code

---
+++ src/Portfolio.Core/Provider/DotEnv.cs
@@ -0,0 +1,24 @@
+namespace Portfolio.Core.Provider;
+
+public static class DotEnv
+{
+	public static void Load()
+	{
+		var root = Directory.GetParent("../../").ToString();
+
+		var dotEnv = Path.Combine(root, ".env");
+
+		if (!File.Exists(dotEnv))
+			return;
+
+		foreach (var line in File.ReadAllLines(dotEnv))
+		{
+			var parts = line.Split('=', StringSplitOptions.RemoveEmptyEntries);
+
+			if (parts.Length != 2)
+				continue;
+
+			Environment.SetEnvironmentVariable(parts[0], parts[1]);
+		}
+	}
+}
--- src/Portfolio.Core/enums/CacheCode.cs
+++ src/Portfolio.Core/enums/CacheCode.cs
@@ -2,15 +2,15 @@ namespace Portfolio.Core.Enums;
 
 public enum CacheCode
 {
-  AboutMe = 01,
-  Activity = 02,
-  Image = 03,
-  Project = 04,
-  ProjectImage = 05,
-  ProjectSkill = 06,
-  S3Object = 07,
-  Skill = 08,
-  SkillImage = 09,
-  User = 10,
-  SystemVariable = 11,
+	AboutMe = 01,
+	Activity = 02,
+	Image = 03,
+	Project = 04,
+	ProjectImage = 05,
+	ProjectSkill = 06,
+	S3Object = 07,
+	Skill = 08,
+	SkillImage = 09,
+	User = 10,
+	SystemVariable = 11,
 }
