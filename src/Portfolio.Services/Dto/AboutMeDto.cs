c68b066a8b5d43646330816c46f47328a862d75b
Lucas Luz <lucasfelipeluz.dev@gmail.com>
2024-03-07 15:23:35 -0300

    feat: add english properties dto

--- src/Portfolio.Services/Dto/AboutMeDto.cs
+++ src/Portfolio.Services/Dto/AboutMeDto.cs
@@ -3,12 +3,15 @@ namespace Portfolio.Services.Dto;
 public class AboutMeDto
 {
 	public int Id { get; set; }
-	public string Name { get; set; } = string.Empty;
-	public string Text { get; set; } = string.Empty;
-	public string JobTitle { get; set; } = string.Empty;
-	public string TelegramLink { get; set; } = string.Empty;
-	public string InstagramLink { get; set; } = string.Empty;
-	public string LinkedinLink { get; set; } = string.Empty;
-	public string GithubLink { get; set; } = string.Empty;
+	public string Name { get; set; }
+	public string Text { get; set; }
+	public string TextEnglish { get; set; }
+	public string JobTitle { get; set; }
+	public string JobTitleEnglish { get; set; }
+	public string TelegramLink { get; set; }
+	public string InstagramLink { get; set; }
+	public string LinkedinLink { get; set; }
+	public string GithubLink { get; set; }
+	public string Address { get; set; }
 	public bool IsAvailable { get; set; }
 }
--- src/Portfolio.Services/Dto/ActivityDto.cs
+++ src/Portfolio.Services/Dto/ActivityDto.cs
@@ -1,12 +1,12 @@
-using Portfolio.Domain.Entities;
-
 namespace Portfolio.Services.Dto;
 
 public class ActivityDto
 {
 	public int Id { get; set; }
 	public string Title { get; set; }
+	public string TitleEnglish { get; set; }
 	public string Description { get; set; }
+	public string DescriptionEnglish { get; set; }
 	public string Icon { get; set; }
 	public DateTime CreatedAt { get; set; }
 }
