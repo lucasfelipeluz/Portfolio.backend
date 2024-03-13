cb1270af99338dc833dac81dfbdb23cc9b3820c3
Lucas Felipe <lucasfelipe281199@gmail.com>
2024-03-12 21:36:43 -0300

    feat: Exception handles

---
+++ src/Portfolio.Core/ExceptionHandles/NotFoundEntityException.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Portfolio.Core.ExceptionHandles
+{
+	public class NotFoundEntityException : Exception
+	{
+		public NotFoundEntityException(string message)
+			: base(message) { }
+
+		public NotFoundEntityException(string message, Exception innerException)
+			: base(message, innerException) { }
+	}
+}
